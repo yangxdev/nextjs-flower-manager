c19ffd92c88d99274265710dc4ab29f30e6b0b23
yangxdev <yangxdev@gmail.com>
2024-05-02 03:02:38 +0200

    Image zoom, styling updates

--- app/components/CalendarSideView.tsx
+++ app/components/CalendarSideView.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { SelectedDateContext } from "../utils/SelectedDateContext";
 import { SelectedDateInfoContext } from "../utils/SelectedDateInfoContext";
+import { Modal } from "antd";
 
 export default function CalendarSideView() {
     // when a date is selected, it will show the orders for that date on the right side
@@ -15,12 +16,24 @@ export default function CalendarSideView() {
 
     const selectedDateInfoArray = selectedDateInfo
         ? Object.values(selectedDateInfo).map((order: any) => ({
-            customerName: order.customerName,
-            customerWechatId: order.customerWechatId,
-            amount: order.amount,
-            photo: order.photo,
-        }))
+              customerName: order.customerName,
+              customerWechatId: order.customerWechatId,
+              amount: order.amount,
+              photo: order.photo,
+          }))
         : [];
+
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [modalImage, setModalImage] = useState("");
+
+    const showModal = (image: string) => {
+        setModalImage(image);
+        setIsModalVisible(true);
+    };
+
+    const handleOk = () => {
+        setIsModalVisible(false);
+    };
 
     return (
         <div className="flex flex-col gap-4 min-w-[22rem]">
@@ -44,7 +57,18 @@ export default function CalendarSideView() {
                             </div>
                             <div className="flex flex-row">
                                 <div className="font-semibold mr-2">Foto:</div>
-                                {order.photo ? <img src={order.photo} alt="order" className="w-40 h-fit rounded-xl" /> : "Nessuna foto"}
+                                {order.photo ? <img src={order.photo} alt="order" className="w-40 h-fit rounded-xl cursor-pointer hover:brightness-90 transition duration-100" onClick={() => showModal(order.photo)} /> : "Nessuna foto"}
+
+                                <Modal open={isModalVisible} onOk={handleOk} onCancel={handleOk} footer={null}>
+                                    <img src={modalImage} className="p-6 -mb-3" alt="order" style={{ width: "100%" }} />
+                                    <div className="additional-info text-center">
+                                        <div className="flex justify-center">
+                                            <div className="font-semibold mr-1">{order.customerName}</div> (@{order.customerWechatId})<br/>
+                                        </div>
+                                        <div>{"€ "}{order.amount}<br /></div>
+                                        <div>{fullDate}</div>
+                                    </div>
+                                </Modal>
                             </div>
                         </div>
                     ))}
@@ -53,3 +77,5 @@ export default function CalendarSideView() {
         </div>
     );
 }
+
+// TODO: mobile view
