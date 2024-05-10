8e381e6d42f362fd2bcb5898ce71d524eebbc3cf
yangxdev <yangxdev@gmail.com>
2024-05-10 14:33:34 +0200

    Side view transition

--- app/components/CalendarSideView.tsx
+++ app/components/CalendarSideView.tsx
@@ -184,119 +184,118 @@ export default function CalendarSideView(props: { orders: any[] }) {
         <>
             <div className="border-b hidden border-[1.5px] md:border-r border-lightBorder"></div>
 
-            {!infoIsEmpty && (
-                <div className="calendar-side-view flex flex-col gap-4 min-w-fit h-full md:overflow-y-auto md:h-[calc(100vh-13rem)]" ref={sideViewRef}>
-                    <div className="mt-2 gap-2 flex flex-row items-center justify-between">
-                        <div className="text-2xl font-bold text-black">{fullDate}</div>
-                        <Button
-                            type="default"
-                            className={`flex items-center w-fit bg-white`}
-                            icon={<FaPlus />}
-                            onClick={() => {
-                                setIsAddModalVisible(true);
-                            }}
-                        ></Button>
-                    </div>
-                    <div className="flex flex-col gap-4 w-full overflow-y-auto">
-                        {selectedDateInfoArray.map((order: any, index: number) => (
-                            <div key={index} className={`info-card gap-1 bg-white flex flex-col justify-between border-2 border-lightBorder rounded-md p-4 ${borderColor(order.soldStatus)} border-l-4`}>
-                                <div className="flex flex-row text-xs opacity-50">{order.id}</div>
-                                <div className="flex flex-row">
-                                    <div className="font-semibold mr-2">Client:</div>
-                                    {order.customerName}
-                                </div>
-                                <div className="flex flex-row">
-                                    <div className="font-semibold mr-2">WeChat ID:</div>
-                                    {order.customerWechatId}
-                                </div>
-                                <div className="flex flex-row">
-                                    <div className="font-semibold mr-2">Advance:</div>
-                                    {"€ " + order.advance}
-                                </div>
-                                <div className="flex flex-row">
-                                    <div className="font-semibold mr-2">Amount:</div>
-                                    {"€ " + order.amount}
-                                </div>
-                                <div className="flex flex-row">
-                                    <div className="font-semibold mr-2">Production cost:</div>
-                                    {"€ " + order.productionCost}
-                                </div>
-                                <div className="flex flex-row">
-                                    <div className="font-semibold mr-2">Status:</div>
-                                    {/* {order.soldStatus === "sold" ? "Sold" : "Non Sold"} */}
-                                    <Form name="">
-                                        <Radio.Group buttonStyle="solid" size="small" className="select-none" onChange={onStatusChange(order.id)} value={order.soldStatus}>
-                                            <ConfigProvider
-                                                theme={{
-                                                    components: {
-                                                        Radio: {
-                                                            buttonSolidCheckedActiveBg: "#F5222D",
-                                                            buttonSolidCheckedBg: "#F5222D",
-                                                            buttonSolidCheckedHoverBg: "#FF4D4F",
-                                                        },
-                                                    },
-                                                }}
-                                            >
-                                                <Radio.Button value="toMake">To make</Radio.Button>
-                                            </ConfigProvider>
-                                            <ConfigProvider
-                                                theme={{
-                                                    components: {
-                                                        Radio: {
-                                                            buttonSolidCheckedActiveBg: "#FAAD14",
-                                                            buttonSolidCheckedBg: "#FAAD14",
-                                                            buttonSolidCheckedHoverBg: "#FFC940",
-                                                        },
-                                                    },
-                                                }}
-                                            >
-                                                <Radio.Button value="toSell">To sell</Radio.Button>
-                                            </ConfigProvider>
-                                            <ConfigProvider
-                                                theme={{
-                                                    components: {
-                                                        Radio: {
-                                                            buttonSolidCheckedActiveBg: "#52C41A",
-                                                            buttonSolidCheckedBg: "#52C41A",
-                                                            buttonSolidCheckedHoverBg: "#73D13D",
-                                                        },
-                                                    },
-                                                }}
-                                            >
-                                                <Radio.Button value="sold">Sold</Radio.Button>
-                                            </ConfigProvider>
-                                        </Radio.Group>
-                                    </Form>
-                                </div>
-                                <div className="flex flex-row">
-                                    <div className="font-semibold mr-2">Photo:</div>
-                                    {order.photo ? <Image src={order.photo} alt="order" width={200} height={200} className="w-40 h-fit rounded-xl cursor-pointer hover:brightness-90 transition duration-100" onClick={() => showZoomModal(order.photo)} /> : "Nessuna Photo"}
-
-                                    <Modal open={isZoomModalVisible} transitionName={isMobile ? "" : undefined} onOk={handleZoomModalClose} onCancel={handleZoomModalClose} footer={null}>
-                                        <Image src={modalImage} className="p-6 -mb-3" height={200} width={200} alt="order" style={{ width: "100%" }} onClick={handleZoomModalClose} />
-                                    </Modal>
-                                </div>
-                                <div className="flex flex-row mt-2 justify-end gap-3">
-                                    <EditForm orderId={order.id} orders={props.orders} />
-                                    <Popconfirm
-                                        title="Delete this order"
-                                        description="Are you sure you want to delete this order? This action cannot be undone."
-                                        onConfirm={() => {
-                                            deleteOrder(order.id);
-                                        }}
-                                        okText="Yes"
-                                        cancelText="No"
-                                    >
-                                        <Button className="flex flex-row gap-1 items-center pr-5" type="primary" danger>
-                                            <MdDelete />
-                                            Delete
-                                        </Button>
-                                    </Popconfirm>
-                                </div>
+            <div className={`calendar-side-view flex flex-col gap-4 min-w-fit h-full md:overflow-y-auto md:h-[calc(100vh-13rem)] ${!infoIsEmpty ? "fade show" : "hide fade"}`} ref={sideViewRef}>
+                <div className="mt-2 gap-2 flex flex-row items-center justify-between">
+                    <div className="text-2xl font-bold text-black">{fullDate}</div>
+                    <Button
+                        type="default"
+                        className={`flex items-center w-fit bg-white`}
+                        icon={<FaPlus />}
+                        onClick={() => {
+                            setIsAddModalVisible(true);
+                        }}
+                    ></Button>
+                </div>
+                <div className="flex flex-col gap-4 w-full overflow-y-auto">
+                    {selectedDateInfoArray.map((order: any, index: number) => (
+                        <div key={index} className={`info-card gap-1 bg-white flex flex-col justify-between border-2 border-lightBorder rounded-md p-4 ${borderColor(order.soldStatus)} border-l-4`}>
+                            <div className="flex flex-row text-xs opacity-50">{order.id}</div>
+                            <div className="flex flex-row">
+                                <div className="font-semibold mr-2">Client:</div>
+                                {order.customerName}
                             </div>
-                        ))}
-                    </div>
-                    {/* <div>
+                            <div className="flex flex-row">
+                                <div className="font-semibold mr-2">WeChat ID:</div>
+                                {order.customerWechatId}
+                            </div>
+                            <div className="flex flex-row">
+                                <div className="font-semibold mr-2">Advance:</div>
+                                {"€ " + order.advance}
+                            </div>
+                            <div className="flex flex-row">
+                                <div className="font-semibold mr-2">Amount:</div>
+                                {"€ " + order.amount}
+                            </div>
+                            <div className="flex flex-row">
+                                <div className="font-semibold mr-2">Production cost:</div>
+                                {"€ " + order.productionCost}
+                            </div>
+                            <div className="flex flex-row">
+                                <div className="font-semibold mr-2">Status:</div>
+                                {/* {order.soldStatus === "sold" ? "Sold" : "Non Sold"} */}
+                                <Form name="">
+                                    <Radio.Group buttonStyle="solid" size="small" className="select-none" onChange={onStatusChange(order.id)} value={order.soldStatus}>
+                                        <ConfigProvider
+                                            theme={{
+                                                components: {
+                                                    Radio: {
+                                                        buttonSolidCheckedActiveBg: "#F5222D",
+                                                        buttonSolidCheckedBg: "#F5222D",
+                                                        buttonSolidCheckedHoverBg: "#FF4D4F",
+                                                    },
+                                                },
+                                            }}
+                                        >
+                                            <Radio.Button value="toMake">To make</Radio.Button>
+                                        </ConfigProvider>
+                                        <ConfigProvider
+                                            theme={{
+                                                components: {
+                                                    Radio: {
+                                                        buttonSolidCheckedActiveBg: "#FAAD14",
+                                                        buttonSolidCheckedBg: "#FAAD14",
+                                                        buttonSolidCheckedHoverBg: "#FFC940",
+                                                    },
+                                                },
+                                            }}
+                                        >
+                                            <Radio.Button value="toSell">To sell</Radio.Button>
+                                        </ConfigProvider>
+                                        <ConfigProvider
+                                            theme={{
+                                                components: {
+                                                    Radio: {
+                                                        buttonSolidCheckedActiveBg: "#52C41A",
+                                                        buttonSolidCheckedBg: "#52C41A",
+                                                        buttonSolidCheckedHoverBg: "#73D13D",
+                                                    },
+                                                },
+                                            }}
+                                        >
+                                            <Radio.Button value="sold">Sold</Radio.Button>
+                                        </ConfigProvider>
+                                    </Radio.Group>
+                                </Form>
+                            </div>
+                            <div className="flex flex-row">
+                                <div className="font-semibold mr-2">Photo:</div>
+                                {order.photo ? <Image src={order.photo} alt="order" width={200} height={200} className="w-40 h-fit rounded-xl cursor-pointer hover:brightness-90 transition duration-100" onClick={() => showZoomModal(order.photo)} /> : "Nessuna Photo"}
+
+                                <Modal open={isZoomModalVisible} transitionName={isMobile ? "" : undefined} onOk={handleZoomModalClose} onCancel={handleZoomModalClose} footer={null}>
+                                    <Image src={modalImage} className="p-6 -mb-3" height={200} width={200} alt="order" style={{ width: "100%" }} onClick={handleZoomModalClose} />
+                                </Modal>
+                            </div>
+                            <div className="flex flex-row mt-2 justify-end gap-3">
+                                <EditForm orderId={order.id} orders={props.orders} />
+                                <Popconfirm
+                                    title="Delete this order"
+                                    description="Are you sure you want to delete this order? This action cannot be undone."
+                                    onConfirm={() => {
+                                        deleteOrder(order.id);
+                                    }}
+                                    okText="Yes"
+                                    cancelText="No"
+                                >
+                                    <Button className="flex flex-row gap-1 items-center pr-5" type="primary" danger>
+                                        <MdDelete />
+                                        Delete
+                                    </Button>
+                                </Popconfirm>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                {/* <div>
                         <Button
                             type="primary"
                             onClick={() => {
@@ -306,8 +305,7 @@ export default function CalendarSideView(props: { orders: any[] }) {
                             Torna su
                         </Button>
                     </div> */}
-                </div>
-            )}
+            </div>
         </>
     );
 }
