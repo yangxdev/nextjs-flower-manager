b80d903650d022c7f466624f3fd496dd5c043618
yangxdev <yangxdev@gmail.com>
2024-05-23 00:51:31 +0200

    Draggable icon position cookies WIP

--- app/components/CalendarSection.tsx
+++ app/components/CalendarSection.tsx
@@ -1,6 +1,6 @@
 "use client";
 import CalendarSideView from "./CalendarSideView";
-import React from "react";
+import React, { useEffect } from "react";
 import Calendar from "./Calendar";
 import OrderForm from "./OrderForm";
 import GlobalConfig from "@/app/app.config";
@@ -9,11 +9,22 @@ import SignOutButton from "./SignOutButton";
 import { Provider } from "react-redux";
 import { store } from "../store/store";
 import Image from "next/image";
+import Draggable from "react-draggable";
+import { useCookies } from "react-cookie";
 
 const defaultLanguage = GlobalConfig.i18n.defaultLanguage || "en";
 const gc = GlobalConfig.i18n.translations[defaultLanguage as keyof typeof GlobalConfig.i18n.translations]?.dashboard;
 
 export default function CalendarSection(props: { orders: any[] }) {
+    const [cookies, setCookies] = useCookies(["iconPosition"]);
+    useEffect(() => {
+        console.log("cookies", cookies.iconPosition);
+    }, [cookies]);
+
+    // if (cookies.iconPosition === undefined) setCookies("iconPosition", JSON.stringify({ x: 220, y: -20 }));
+    const defaultIconPosition = cookies.iconPosition ? cookies.iconPosition : { x: 220, y: -20 };
+    // BUG: icon position not aligned after page loading
+
     return (
         <>
             <Provider store={store}>
@@ -36,14 +47,17 @@ export default function CalendarSection(props: { orders: any[] }) {
                             height={40}
                         ></Image>
                     </div> */}
-                    <div className="div absolute z-10 right-[24px] md:right-[16px] -top-[6px]">
-                        <Image
-                            src="/others/capy_front.png"
-                            alt="capybara"
-                            width={40}
-                            height={40}
-                        ></Image>
-                    </div>
+                    <Draggable
+                        key={cookies.iconPosition.x + "," + cookies.iconPosition.y}
+                        defaultPosition={defaultIconPosition}
+                        onStop={(e, data) => {
+                            setCookies("iconPosition", JSON.stringify({ x: data.x, y: data.y }));
+                        }}
+                    >
+                        <div className="capy cursor-pointer absolute z-10" style={{}}>
+                            <Image src="/others/capy_front.png" alt="capybara" width={40} height={40}></Image>
+                        </div>
+                    </Draggable>
                     <Calendar orders={props.orders} />
                     <CalendarSideView orders={props.orders} />
                 </div>
