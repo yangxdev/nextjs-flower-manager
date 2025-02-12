fd43e1c835b856796c33089a19efaaaa2a828465
yangxdev <yangxdev@gmail.com>
2025-02-12 16:26:54 +0100

    Enhance error handling in getAllOrders function and update Prisma client version to 5.22.0

--- app/api/database/get_orders/orders.ts
+++ app/api/database/get_orders/orders.ts
@@ -1,6 +1,11 @@
 import { prisma } from "@/app/api/_base";
 
 export async function getAllOrders() {
-    const orders = await prisma.order.findMany();
-    return orders;
+    try {
+        const orders = await prisma.order.findMany();
+        return orders;
+    } catch (error) {
+        console.error("Error fetching orders:", error);
+        throw new Error("Failed to fetch orders");
+    }
 }
